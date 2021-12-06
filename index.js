669c13bf986ba1c6c3588705406b094de4151ee8
Neto322 <manuel123317@hotmail.com>
2021-12-06 15:15:01 -0800

    Updates

---
+++ index.js
@@ -0,0 +1,89 @@
+import * as THREE from './js/build/three.module.js';
+import {GLTFLoader} from './js/examples/jsm/loaders/GLTFLoader.js'
+import { VRButton } from './js/examples/jsm/webxr/VRButton.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.xr.enabled = true;
+document.body.appendChild( renderer.domElement );
+
+window.addEventListener('resize', onWindowResize);
+
+document.body.appendChild(VRButton.createButton(renderer));
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//const cube = new THREE.Mesh( geometry, material );
+//scene.add( cube );
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 0.9 );
+scene.add( light );
+
+const loader = new GLTFLoader();
+
+
+loader.load( './models/warthongo y el chif.gltf', 
+ ( gltf ) => {
+
+  scene.add( gltf.scene);
+},
+
+
+// called while loading is progressing
+( xhr ) => {
+
+  console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+},
+// called when loading has errors
+( error ) => {
+
+  console.log( 'An error happened' );
+
+});
+
+
+camera.position.z = 3.5;
+camera.position.y = 1;
+camera.position.x = -1;
+
+camera.rotation.x = -0.2;
+camera.rotation.y = -0.5;
+
+
+var jsonLoader = new THREE.JSONLoader();
+jsonLoader.load("models/otro.json", addModelToScene);
+
+function addModelToScene(geometry, materials) {
+    var material = new THREE.MeshFaceMaterial(materials);
+    var object = new THREE.Mesh(geometry, material);
+    object.scale.set(10, 10, 10);
+    scene.add(object);
+}
+
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function animate() {
+  //cube.rotation.x += 0.01;
+  //cube.rotation.y += 0.01;
+	// requestAnimationFrame( animate );
+	// renderer.render( scene, camera );
+
+  renderer.setAnimationLoop( render );
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+animate();
